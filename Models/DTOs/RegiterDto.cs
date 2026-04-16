1eb4807260153e8336e247937364e9df3c4101f2
andriybriliant <andrii.bryliant.inq@gmail.com>
2026-04-16 17:11:31 +0200

    Added register dto

---
+++ Models/DTOs/RegiterDto.cs
@@ -0,0 +1,8 @@
+namespace LibraryCatalogAPI.Models.DTOs;
+
+public class RegisterDto
+{
+    public string NameSurname { get; set; } = string.Empty;
+    public string Username { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}

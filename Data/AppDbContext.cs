fc7fdd75c1a644b117649e2beda03221cb768a4c
andriybriliant <andrii.bryliant.inq@gmail.com>
2026-04-16 17:18:18 +0200

    Updated dependencies, added AppDbContext

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+using LibraryCatalogAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryCatalogAPI.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options){}
+
+    public DbSet<Book> Books { get; set; }
+    public DbSet<User> Users { get; set; }
+}

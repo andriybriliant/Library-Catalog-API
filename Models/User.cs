1e1a97a6204002b5035f8c176d81d9945f0737e6
andriybriliant <andrii.bryliant.inq@gmail.com>
2026-04-16 17:01:34 +0200

    Added user and book models, updated gitignore

---
+++ Models/User.cs
@@ -0,0 +1,10 @@
+namespace LibraryCatalogAPI.Models;
+
+public class User
+{
+    public Guid Id { get; set; }
+    public string Username { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = string.Empty;
+    public string? RefreshToken { get; set; }
+    public DateTime? RefreshTokenExpiryTime { get; set; }
+}
